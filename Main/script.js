40849fe57a093440f039cde2b8e4b16ab62495ab
Jesús Connolly <jesusconnolly14@gmail.com>
2024-04-29 15:01:59 -0500

    Finish

--- Main/script.js
+++ Main/script.js
@@ -1,32 +1,4 @@
 
-// const url = 'https://chinese-food-db.p.rapidapi.com/';
-// const options = {
-//     method: 'GET',
-//     headers: {
-//         'X-RapidAPI-Key': 'a1068c7031msh2b195a008b98f24p147622jsn52b18b5cb4d7',
-//         'X-RapidAPI-Host': 'chinese-food-db.p.rapidapi.com'
-//     }
-// };
-
-// fetch(url, options) // Agrega options para incluir los encabezados de RapidAPI
-//     .then(response => response.json())
-//     .then(data => {
-//         // Filtrar solo los primeros 10 elementos
-//         const firstTen = data.slice(0, 10);
-
-//         // Iterar sobre los primeros 10 elementos y mostrar los detalles específicos
-//         firstTen.forEach(item => {
-//             console.log("ID:", item.id);
-//             console.log("Título:", item.title);
-//             console.log("Dificultad:", item.difficulty);
-//             console.log("Imagen:", item.image);
-//             console.log("------------------------");
-//         });
-//     })
-//     .catch(err => console.log(err));
-
-
-
 
 document.addEventListener("DOMContentLoaded", function() {
     const url = 'https://chinese-food-db.p.rapidapi.com/';
